8c95786bd912b9c8dda74fa9603e4f8ede72b5af
Andre Yu <Xmaxter213@gmail.com>
2023-07-20 05:24:37 +0800

    Added animation & base (floor)

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ pygame.font.init()
 WIN_WIDTH = 400
 WIN_HEIGHT = 600
 
-BIRD_IMGS = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird_0.png")))
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("D:\\Programming\\Python\\FinalsFlappyBird\\flappybird_ai\\imgs", "bird_0.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("D:\\Programming\\Python\\FinalsFlappyBird\\flappybird_ai\\imgs", "bird_1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("D:\\Programming\\Python\\FinalsFlappyBird\\flappybird_ai\\imgs", "bird_2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("D:\\Programming\\Python\\FinalsFlappyBird\\flappybird_ai\\imgs", "bird_3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("D:\\Programming\\Python\\FinalsFlappyBird\\flappybird_ai\\imgs", "pipe.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("D:\\Programming\\Python\\FinalsFlappyBird\\flappybird_ai\\imgs", "bg.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("D:\\Programming\\Python\\FinalsFlappyBird\\flappybird_ai\\imgs", "base.png")))
 
 STAT_FONT = pygame.font.SysFont("comicsans", 30)
 
@@ -18,6 +19,7 @@ class Bird:
   IMGS = BIRD_IMGS
   MAX_ROTATION = 25
   ROTATION_VELOCITY = 20
+  ANIMATION_TIME = 5
 
   def __init__(self, x, y):
     self.x = x
@@ -26,7 +28,8 @@ class Bird:
     self.tick_count = 0
     self.velocity = 0
     self.height = self.y
-    self.img = self.IMGS
+    self.img_count = 0
+    self.img = self.IMGS[0]
 
   def jump(self):
     self.velocity = -6.5
@@ -54,6 +57,29 @@ class Bird:
         self.tilt -= self.ROTATION_VELOCITY
 
   def draw(self, win):
+    self.img_count += 1
+
+    if self.img_count <= self.ANIMATION_TIME:
+        self.img = self.IMGS[0]
+    elif self.img_count <= self.ANIMATION_TIME*2:
+        self.img = self.IMGS[1]
+    elif self.img_count <= self.ANIMATION_TIME*3:
+        self.img = self.IMGS[2]
+    elif self.img_count <= self.ANIMATION_TIME*4:
+        self.img = self.IMGS[3]
+    elif self.img_count <= self.ANIMATION_TIME*5:
+        self.img = self.IMGS[2]
+    elif self.img_count <= self.ANIMATION_TIME*5:
+        self.img = self.IMGS[1]
+    elif self.img_count == self.ANIMATION_TIME*5 + 1:
+        self.img = self.IMGS[0]
+        self.img_count = 0
+
+    # so when bird is nose diving it isn't flapping
+    if self.tilt <= -80:
+        self.img = self.IMGS[1]
+        self.img_count = self.ANIMATION_TIME*2
+
     rotated_image = pygame.transform.rotate(self.img, self.tilt)
     new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
     win.blit(rotated_image, new_rect.topleft)
@@ -104,11 +130,36 @@ class Pipe:
     if t_point or b_point:
       return True
 
-def draw_window(win, birds, pipes, score):
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+def draw_window(win, birds, pipes, base, score):
   win.blit(BG_IMG, (0, 0))
   for pipe in pipes:
     pipe.draw(win)
 
+  base.draw(win)
+  
   text = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
   win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
 
@@ -131,6 +182,7 @@ def eval_gnomes(gnomes, config):
     ge.append(gnome)
 
   pipes = [Pipe(350), Pipe(600), Pipe(850)]
+  base = Base(550)
   win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
   clock = pygame.time.Clock()
 
@@ -192,12 +244,13 @@ def eval_gnomes(gnomes, config):
       pipes.remove(pipe_to_remove)
 
     for x, bird in enumerate(birds):
-      if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
+      if bird.y + bird.img.get_height() >= 550 or bird.y < 0:
         birds.pop(x)
         nets.pop(x)
         ge.pop(x)
 
-    draw_window(win=win, birds=birds, pipes=pipes, score=score)
+    base.move()
+    draw_window(win=win, birds=birds, pipes=pipes, base=base, score=score)
 
   # pygame.quit()
   # quit()
